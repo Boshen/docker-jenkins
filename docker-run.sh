881c091443e65b0b54b4963348a0b4c78fe6ef25
Boshen <boshenc@gmail.com>
2020-07-13 15:44:34 +0800

    update

---
+++ docker-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# upgrade - build the latest image
+docker build --no-cache --pull --rm --tag jenkins-docker:latest .
+
+docker run --name jenkins -d -v jenkins-data:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 -p 50000:50000 jenkins-docker
